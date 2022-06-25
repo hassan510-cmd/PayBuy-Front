732720ef85c4ff0cde1abed8b7106ee702523599
hassan510-cmd <hassanmahmoud607@gmail.com>
2022-06-25 13:37:55 +0200

    geneate habits intervals

--- src/routes/Content/HabitTracker.jsx
+++ src/routes/Content/HabitTracker.jsx
@@ -5,31 +5,18 @@ import Popup from 'reactjs-popup';
 import { SwipeableList, SwipeableListItem } from '@sandstreamdev/react-swipeable-list';
 import { PencilSquare, Trash } from 'react-bootstrap-icons';
 import { useNavigate } from 'react-router-dom';
-import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
-import { useFilePicker } from 'use-file-picker';
 import axios from 'axios';
 import { BaseUrl } from '../../baseUrl/BaseUrl';
 export default function HabitTracker() {
     const [Days, setDays] = useState([])
-    const [value, onChange] = useState(new Date());
     const navigate = useNavigate();
-    const [openFileSelector, { filesContent, loading }] = useFilePicker({
-        accept: '.txt',
-    });
 
     useEffect(() => {
         axios.get(`${BaseUrl}/habits`).then(resp => {
             setDays(resp.data)
         })
-        // let data = [
-        //     { id: 1, day: 1, state: true },
-        //     { id: 2, day: 10, state: true },
-        //     { id: 3, day: 15, state: true },
-        //     { id: 4, day: 18, state: true },
-        //     { id: 5, day: 19, state: true },
-        // ]
-        // setDays(data)
+
     }, [])
 
     const toggleState = (obj, index) => {
@@ -37,9 +24,18 @@ export default function HabitTracker() {
         Days[index] = obj
         setDays([...Days])
     }
+    const addHabit = (e) => {
+        e.preventDefault()
+        console.log(e.target.name)
+        axios.post(`${BaseUrl}/add-habit`, {
+            name: e.target.name.value
+        }).then(resp => {
+            setDays([resp.data, ...Days])
+        })
+    }
     return (
         <div>
-            <button className='btn btn-danger'
+            {/* <button className='btn btn-danger'
                 onClick={() => openFileSelector()}>
                 Select files </button>
             {filesContent.map((file, index) => (
@@ -48,7 +44,7 @@ export default function HabitTracker() {
                     <div key={index}>{file.content}</div>
                     <br />
                 </div>
-            ))}
+            ))} */}
             <Popup defaultOpen={false} modal nested trigger={
                 <span>
 
@@ -57,15 +53,21 @@ export default function HabitTracker() {
 
             } contentStyle={{ width: "85%", borderRadius: "30px" }} position="top right">
                 <div className='container mb-3 p-3'>
-                    <form id="source-form" className='from-group' >
+                    <form id="source-form" className='from-group' onSubmit={addHabit}>
                         <label htmlFor="name">Name</label>
                         <input type="text" name='name' className='form-control my-2' placeholder='Habit Name' />
-                        {/* <label htmlFor="from_date">Start From</label>
-                        <input type="date" name='from_date' className='form-control mb-2' placeholder='default value' />
-                        <label htmlFor="to_date">To</label>
-                        <input type="date" name='to_date' className='form-control mb-2' placeholder='default value' />
-                        <Calendar onChange={onChange} value={value} />
-                    */}
+                        <div className='d-flex justify-content-between'>
+                            <div>
+                                <label htmlFor="from_date">Start From</label>
+                                <input type="date" name='from_date' className='form-control mb-2' placeholder='default value' />
+                            </div>
+                            <div>
+                                <label htmlFor="to_date">To</label>
+                                <input type="date" name='to_date' className='form-control mb-2' placeholder='default value' />
+                            </div>
+                        </div>
+                        {/* <Calendar onChange={onChange} value={value} /> */}
+
                         <button type='submit' className='btn btn-success form-control'>add</button>
                     </form>
                 </div>
@@ -78,7 +80,7 @@ export default function HabitTracker() {
             }}>
                 <SwipeableList  >
                     {Days.map((obj, index) => (
-                        <div className='mb-2 shadow-sm' onClick={() => navigate(`/habits-tracker-details`)}>
+                        <div className='mb-2 shadow-sm' onClick={() => navigate(`/habits-tracker-details/?hid=${obj.id}`)}>
                             <SwipeableListItem key={obj.id}
                                 swipeLeft={{
                                     content: <div className=" list-group-item rounded-3 shadow-sm
--- src/routes/Content/HabitTrackerDetails.jsx
+++ src/routes/Content/HabitTrackerDetails.jsx
@@ -1,14 +1,16 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
 import { BaseUrl } from '../../baseUrl/BaseUrl'
 import "../../style/flip.css"
 export default function HabitTrackerDetails() {
     const [Days, setDays] = useState([])
-
+    const search = useLocation().search
+    const habitId = new URLSearchParams(search).get('hid')
     useEffect(() => {
         axios.get(`${BaseUrl}/habit-intervals`, {
             params: {
-                habit_id: 1,
+                habit_id: habitId,
             }
         }).then(resp => {
             console.log(resp.data)
@@ -21,9 +23,37 @@ export default function HabitTrackerDetails() {
         Days[index] = obj
         setDays([...Days])
     }
+
+    const generateHabitPeriod = (e) => {
+        e.preventDefault()
+        axios.post(`${BaseUrl}/generate-habit-period`, {
+            habit_id: habitId,
+            date_from: e.target.date_from.value,
+            date_to: e.target.date_to.value
+        }).then(resp => {
+            console.log(resp.data)
+            setDays(resp.data)
+        })
+    }
     return (
         <div>
-            <div className='d-flex  container flex-wrap justify-content-start'>
+            <form onSubmit={generateHabitPeriod} className="mb-2">
+                <div className='d-flex justify-content-between container flex-wrap'>
+                    <div>
+                        {/* <label htmlFor="from_date">Start From</label> */}
+                        <input type="date" name='date_from' className='form-control mb-2' placeholder='default value' />
+                    </div>
+                    <div>
+                        {/* <label htmlFor="to_date">To</label> */}
+                        <input type="date" name='date_to' className='form-control mb-2' placeholder='default value' />
+                    </div>
+                    <div>
+                        <button className='btn btn-dark ' type='submit'>generate</button>
+                    </div>
+                </div>
+            </form>
+            <div className='d-flex  container flex-wrap justify-content-between'>
+
                 {Days.map((day, index) => (
 
 
